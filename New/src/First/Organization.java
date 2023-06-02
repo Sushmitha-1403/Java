2a1a8cb8a000020024f0f7c92bdb5ccc1e070073
github_sushmitha-1403 <github_sushmithakundali.s.1403@gmail.com>
2023-06-02 00:37:07 -0500

    Polymorphism

---
+++ New/src/First/Organization.java
@@ -0,0 +1,17 @@
+package First;
+
+abstract class Department {
+	abstract int getDepartmentSize();
+
+}
+class Organization extends Department{
+	int getDepartmentSize() {
+		return 999;
+	}
+	public static void main (String args[]) {
+		Department size = new Organization();
+		
+		System.out.println("Department Size:"+size.getDepartmentSize());
+	}
+	
+}
---
+++ New/src/First/Product.java
@@ -0,0 +1,27 @@
+package First;
+
+public class Product {
+	private static double price;
+	private static double quantity;
+	
+	public static void setPrice(double p) {
+		price=p;
+	}
+	
+	public static double getPrice() {
+		return price;
+	}
+	public static double getPrice(double quantity) {
+		return(quantity);
+	}
+	
+	public static void main(String args[]) {
+		Product.setPrice(34.345);
+		System.out.println("Price:"+Product.getPrice());
+		System.out.println("Quantity:"+Product.getPrice(3444.345));
+		
+	}
+	
+	
+
+}
