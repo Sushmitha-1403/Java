52d95aae888525a06319728137253c0d7d3828d1
github_sushmitha-1403 <github_sushmithakundali.s.1403@gmail.com>
2023-06-19 14:53:36 -0500

    `Lambdas and Streams

---
+++ Week3/src/d06162023/StringSort.java
@@ -0,0 +1,25 @@
+package d06162023;
+
+import java.util.Comparator;
+import java.util.stream.Stream;
+
+public class StringSort {
+	
+	public static void main(String args[]) {
+		Stream<String> st=Stream.of("Serendipity",
+				"Quixotic",
+				"Mellifluous",
+				"Bucolic",
+				"Effervescent",
+				"Labyrinthine",
+				"Epiphany",
+				"Zephyr",
+				"Ephemeral",
+				"Cascade");
+		
+		st.sorted(Comparator.comparingInt(String::length))
+			.forEach(word->System.out.println(word+":"+word.length()));
+		
+
+}
+}
